18e0f5684e952e2421b399d48b9aa60c191ef8c8
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2021-01-06 02:39:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,8 +12,8 @@ def key_for_min_value(name_hash)
      name_hash.each do |key,value|
        puts "#{key} #{value}"
        if value < lowest_value
-         lowest_value == value
-         lowest_key == key
+         lowest_value = value
+         lowest_key = key
        end
 
     end
